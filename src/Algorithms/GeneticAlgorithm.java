473a6887a284714c6bf730224d4c2f8946b46730
Jorge Garcia <silversoldier9@hotmail.com>
2016-05-22 16:48:44 +0200

    Calculate Fitness on generation

--- src/Algorithms/GeneticAlgorithm.java
+++ src/Algorithms/GeneticAlgorithm.java
@@ -1,6 +1,6 @@
 package Algorithms;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
@@ -10,10 +10,11 @@ import main.Problema;
 import main.Util;
 
 public class GeneticAlgorithm extends Algorithms {
-	
+
+	private static final int ELITE_SIZE = 4;
 	private static final int GENES_NUMBER = 3;  // Total number of genes
 	private static final int GEN_DIMENSION = 10; // Dimension for each gene
-	private static final int MAX_GENERATIONS = 5; 
+	private static final int MAX_GENERATIONS = 5;
 	private static final int POPULATION_SIZE = 10; // Initial population size
 	private PriorityQueue<Individuo> population = new PriorityQueue<Individuo>();
 	private Problema problem;
@@ -22,7 +23,7 @@ public class GeneticAlgorithm extends Algorithms {
 		this.problem = p;
 		this.population = generateInitialPopulation();
 	}
-	
+
 	/*
 	 * Generating a random population of size N
 	 * Returns a priority queue of N individuals
@@ -37,49 +38,55 @@ public class GeneticAlgorithm extends Algorithms {
 			newIndividual.setFitness(generateFitness(randomCircle));
 			new_population.add(newIndividual);
 		}
-		
+
 		return new_population;
 	}
-	 
+
 	@Override
 	public Circulo BestSolution(Problema p) {
 		Circulo bestCircle = new Circulo(0, 0, 0);
 		Individuo bestGenerationIndividual;
-		
+
 		for (int i = 0; i < MAX_GENERATIONS; i++) {
 			generateAndUpdateNewPopulation(); // Assign new population after generated it
 			// TODO: Cada nueva generacion coger el mejor circulo
 			// Si el circulo es mejor que el encontrado hasta la fecha, actualizarlo.
 			bestGenerationIndividual = this.population.peek();
 		}
-		
+
 		return bestCircle;
 	}
-	
 
 	public void generateAndUpdateNewPopulation() {
 		int createdChromosomes = 0;
 		PriorityQueue<Individuo> newPopulation = new PriorityQueue<Individuo>();
 		float[] accumProbability = new float[POPULATION_SIZE];
 		float totalFitness = sumAllFitness();
-		
-		while (newPopulation.size() < POPULATION_SIZE) {
-			
-			accumProbability = calculateAccumProbability(totalFitness); 
+
+		// Merge population
+		while (newPopulation.size() < POPULATION_SIZE - ELITE_SIZE) {
+
+			accumProbability = calculateAccumProbability(totalFitness);
 			Pair newPair = pickCouples(accumProbability); // returns list of size 2 chromosomes to work with them
-			
-			newPair.cross();  
+
+			newPair.cross();
 			newPair.mutate();
-			
+			newPair.calculateFitness();
 			newPopulation.addAll(newPair.toList());
-			// Calculate Fitness
 		}
-		
+
+		// Get elite from old population and add it to new population
+		Individuo topEliteElement;
+		for (int i = 0; i < ELITE_SIZE; i++) {
+			if (population.size() > 0) {
+				topEliteElement = population.poll();
+				newPopulation.add(topEliteElement);
+			}
+		}
+
 		this.population = newPopulation;
 	}
-	
-	
-	
+
 	private Pair pickCouples(float[] accumProbability) {
 		// TODO Auto-generated method stub
 		return null;
@@ -92,18 +99,18 @@ public class GeneticAlgorithm extends Algorithms {
 		float accumProbability[] = new float[POPULATION_SIZE];
 		float tmpAccumProbability;
 		float fitness;
-		
+
 		tmpAccumProbability = 0;
-		
+
 		for (int j = 0; j < POPULATION_SIZE; j++) {
 			fitness = this.population.get(j).getFitness();
 			tmpAccumProbability += fitness / totalFitness;
 			accumProbability[j] = tmpAccumProbability;
 		}
-		
+
 		return accumProbability;
 	}
-	
+
 	/*
 	 * Roulette: Select a random individual that fills the condition:
 	 */
@@ -115,15 +122,15 @@ public class GeneticAlgorithm extends Algorithms {
 		while (index < POPULATION_SIZE && !found) {
 			if (accumProbability[index] < randomProbability) {
 				index++;
-			}	
+			}
 			else {
 				found = true;
 			}
 		}
-		
+
 		return index;
 	}
-	
+
 	/*
 	 * Cross 2 chromosomes and return a new indivudal
 	 */
@@ -132,9 +139,9 @@ public class GeneticAlgorithm extends Algorithms {
 		float tmpFitness;
 		Circulo tmpCircle;
 		String tmpCromosome, chromosomeA, chromosomeB;
-	
+
 		crossPoint = crossPoint();
-		
+
 		chromosomeA = this.population.get(indexChromoA).getCromosoma();
 		chromosomeB = this.population.get(indexChromoB).getCromosoma();
 
@@ -145,9 +152,9 @@ public class GeneticAlgorithm extends Algorithms {
 
 		return new Individuo(tmpCromosome, tmpFitness);
 	}
-	
-	
-	
+
+
+
 	/*
 	public ArrayList<Individuo> generateNewPopulationOf(int size, float totalFitness) {
 		int insertedIndivuduals;
@@ -155,20 +162,20 @@ public class GeneticAlgorithm extends Algorithms {
 
 		int indexChromosomeA;
 		int indexChromosomeB;
-		
+
 		Individuo newCrossedIndividual;
 		ArrayList<Individuo> newPopulation = new ArrayList<Individuo>();
-		
-	
-		accumProbability = calculateAccumProbability(totalFitness); 
-		
+
+
+		accumProbability = calculateAccumProbability(totalFitness);
+
 		insertedIndivuduals = 0;
-		
+
 		// Iterar hasta que tengamos N nuevos individuos.
 		while (insertedIndivuduals < size) {
-						
-			indexChromosomeA = selectIndividualIndex(accumProbability); // Indice aleatorio del cromosoma 1: Llamar Función que te devuelve un indice del cromosoma
-			indexChromosomeB = selectIndividualIndex(accumProbability); // Indice aleatorio del cromosoma 2: Llamar Función que te devuelve un indice del cromosoma
+
+			indexChromosomeA = selectIndividualIndex(accumProbability); // Indice aleatorio del cromosoma 1: Llamar Funciï¿½n que te devuelve un indice del cromosoma
+			indexChromosomeB = selectIndividualIndex(accumProbability); // Indice aleatorio del cromosoma 2: Llamar Funciï¿½n que te devuelve un indice del cromosoma
 
 			if (canCrossChromosomes()) {
 				newCrossedIndividual = generateCrossedIndividual(indexChromosomeA, indexChromosomeB);
@@ -181,11 +188,11 @@ public class GeneticAlgorithm extends Algorithms {
 				// Mutar los cromosomas
 			}
 		}
-		
+
 		return newPopulation;
 	}
 	*/
-		
+
 	private float generateFitness(Circulo c) {
 		if (this.problem.esSolucion(c)) {
 			return (float) c.getRadio();
@@ -193,10 +200,10 @@ public class GeneticAlgorithm extends Algorithms {
 			return 0;
 		}
 	}
-	
+
 	public void printPopulation() {
 		String individual = "";
-		
+
 		for (Individuo e : population) {
 			individual = "Cromosoma: ";
 			individual += e.getCromosoma();
@@ -206,15 +213,15 @@ public class GeneticAlgorithm extends Algorithms {
 		}
 	}
 
-	// Sum all the individuals fitness score 
+	// Sum all the individuals fitness score
 	public float sumAllFitness() {
 		float total = 0;
 
 		for (Individuo e : population) {
 			total += e.getFitness();
 		}
-		
+
 		return total;
 	}
-	
+
 }
